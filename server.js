b649499e4d652a297f4d9a8dd292936157b59217
Rieri-Syame <paolomiguel0409@gmail.com>
2025-04-25 00:11:23 +0800

    Update server.js

--- server.js
+++ server.js
@@ -1,13 +1,12 @@
-// server.js
-const express = require('express');
-const cors = require('cors');
-const fetch = require('node-fetch');
+// server.js (ES Module format)
+import express from 'express';
+import cors from 'cors';
+import fetch from 'node-fetch';
 
 const app = express();
 app.use(cors());
 app.use(express.json({ limit: '10mb' }));
 
-// Endpoint for your front-end to POST to
 app.post('/upload', async (req, res) => {
   const targetUrl = 'https://script.google.com/macros/s/AKfycby1eepW4cUzuvWjM_r61A848NLQmXI9PumEW0j6mxdPQgAmLelaQOZolrKkB83cx_b0/exec';
   try {
@@ -19,7 +18,6 @@ app.post('/upload', async (req, res) => {
     const text = await response.text();
     res.send(text);
   } catch (err) {
-    console.error('Proxy error:', err);
     res.status(500).send('Proxy error: ' + err.message);
   }
 });
